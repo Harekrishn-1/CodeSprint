06116ca67c393f0c786cc0db06f2fe139c3bd1e8
Harekrishn-1 <harekrishnpachauri@gmail.com>
2026-03-20 17:54:20 +0530

    Codesprint project completed

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const main =  require('./src/config/db')
+const cookieParser =  require('cookie-parser');
+const authRouter = require("./src/routes/userAuth");
+const redisClient = require('./src/config/redis');
+const problemRouter = require("./src/routes/problemCreator");
+const submitRouter = require("./src/routes/submit")
+const aiRouter = require("./src/routes/aiChatting")
+const videoRouter = require("./src/routes/videoCreator");
+const cors = require('cors')
+
+// console.log("Hello")
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true 
+}))
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/user',authRouter);
+app.use('/problem',problemRouter);
+app.use('/submission',submitRouter);
+app.use('/ai',aiRouter);
+app.use("/video",videoRouter);
+
+
+const InitalizeConnection = async ()=>{
+    try{
+
+        await Promise.all([main(),redisClient.connect()]);
+        console.log("DB Connected");
+        
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server listening at port number: "+ process.env.PORT);
+        })
+
+    }
+    catch(err){
+        console.log("Error: "+err);
+    }
+}
+
+
+InitalizeConnection();
+
